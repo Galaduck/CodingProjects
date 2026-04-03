f1b09e4f794a3eb74b93a05b569c4fa83381f30d
Aiden <ai6543r@gmail.com>
2026-04-03 12:21:19 -0400

    Added ceaser encryption and decryption
    I created two functions. One encrypts a message using the ceaser cypher, and one decrypts a message.

--- Projects/2026/Ciphers/ceaser.py
+++ Projects/2026/Ciphers/ceaser.py
@@ -1,3 +1,32 @@
 lowercase = 'abcdefghijklmnopqrstuvwxyz'
 uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+# Encrypts a message
+def EncryptCeaser(message, shift):
+    EncryptedMessage = ''
+    for x in message:
+        if x in lowercase:
+            EncryptedMessage += lowercase[(lowercase.index(x) + shift) % 26]
+
+        elif x in uppercase:
+            EncryptedMessage += uppercase[(uppercase.index(x) + shift) % 26]
+
+        else:
+            EncryptedMessage += x
+
+    return EncryptedMessage
+        
+def DecryptCeaser(message, shift):
+    DecryptedMessage = ''
+    for x in message:
+        if x in lowercase:
+            DecryptedMessage += lowercase[(lowercase.index(x) - shift) % 26]
+
+        elif x in uppercase:
+            DecryptedMessage += uppercase[(uppercase.index(x) - shift) % 26]
+
+        else:
+            DecryptedMessage += x
+
+    return DecryptedMessage
+    
